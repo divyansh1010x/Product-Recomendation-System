b7c54adf6263b8b9228391b2c48af3cecffd441d
Agam77055 <agamstudy2005@gmail.com>
2024-11-08 14:24:38 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../src/pages/homePage';
-import DummyPage from '../src/pages/dummyPage';
-import LoginPage from '../src/pages/loginPage';
-import SignupPage from '../src/pages/signupPage';
+import DummyPage from '../src/pages/dummyPage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path='/dummy' element={<DummyPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   );
